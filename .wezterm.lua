2c5e8fe5635bd2d9c03f759af779fcd291bf7656
Bilguun Ochirbat <bilguun0203@gmail.com>
2023-11-25 16:33:09 +0800

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ config.window_background_opacity = 0.9
 config.font_size = 10
 
 config.window_frame = {
-  font = wezterm.font { family = 'JetBrainsMono Nerd Font', weight = 'Bold' },
+  font = wezterm.font { family = 'JetBrains Mono', weight = 'Bold' },
   font_size = 10,
   active_titlebar_bg = '#333333',
   inactive_titlebar_bg = '#333333',
